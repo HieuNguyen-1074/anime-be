2b651bcf4651eae919d5d50ac64fdc9252b5d9b9
Hieu Nguyen <hieunv2@bssd.vn>
2024-08-19 08:39:21 +0700

    update

--- controllers/emblems.js
+++ controllers/emblems.js
@@ -22,16 +22,11 @@ const getEmblemById = async (req, res) => {
   const embId = req.params.embId;
 
   if (!embId) res.status(404).json('couldnt found');
-  const emblem = Emblem.findById(embId);
+  let emblem = await Emblem.findById(embId);
   const categories = await Category.find({});
   const cards = await CardModel.find({});
   if (!emblem) res.status(404).json('couldnt found');
-  emblem.categories = categories.map((category) => {
-    const cards = cards.filter(
-      (card) =>
-        card.categoryId === category._id.$oid && card.emblemIds.includes(embId)
-    );
-  });
+  console.log(emblem, categories);
 
   //   if (!mongoose.isValidObjectId(req.params.id)) {
   //     res.status(400);
@@ -42,7 +37,22 @@ const getEmblemById = async (req, res) => {
   //     res.status(404);
   //     throw new Error('Card not found');
   //   }
-  res.status(200).json(emblem);
+  const ct = categories.map((category) => {
+    const cardsMatch = cards.filter(
+      (card) =>
+        card.categoryId === category._id.$oid && card.emblemIds.includes(embId)
+    );
+
+    return {
+      ...category._doc,
+      cards: cardsMatch,
+    };
+  });
+
+  res.status(200).json({
+    ...emblem._doc,
+    categories: ct,
+  });
 };
 
 module.exports = { getEmblems, getEmblemById };
